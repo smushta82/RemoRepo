22497e21009fb242b8e7305706e50145abace218
Sal Mush <smushta82@gmail.com>
2021-04-08 20:00:06 -0400

    Hello World

---
+++ Maven201201E/src/main/java/util/Source.java
@@ -0,0 +1,9 @@
+package util;
+
+public class Source {
+	
+	public void  m1() {
+		System.out.println("Hi");
+	}
+
+}
---
+++ Maven201201E/src/test/java/com/SmokeTest.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.junit.Test;
+
+import util.Source;
+
+public class SmokeTest {
+	
+	@Test
+	public void test1() {
+		Source s = new Source();
+		s.m1();
+	}
+
+}
